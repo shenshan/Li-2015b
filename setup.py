95375965b2d89c314c4a9f8375df1a56fa547df2
shenshan <shenshanpku@gmail.com>
2020-03-24 16:26:14 -0700

    Add pynwb into requirements.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='li2015b_pipeline',
-    version='0.0.0',
+    version='0.0.1',
     description='Datajoint schemas for Li 2015b',
     author='Vathes',
     author_email='support@vathes.com',
     packages=find_packages(exclude=[]),
-    install_requires=['datajoint>=0.12.dev7'],
+    install_requires=['datajoint>=0.12', 'pynwb'],
 )
